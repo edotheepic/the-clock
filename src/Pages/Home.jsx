b3e2bd462239804ccf0e9eb982391b5a678e6202
edotheepic <86707396+edotheepic@users.noreply.github.com>
2024-11-04 21:44:35 +0700

    Create Homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,22 @@
 export default function Home(params) {
     
     return(
-        <div>HOMEPAGE</div>
+        <div className="p-10 text-8xl font-semibold font-mono tracking-widest text-slate-800">
+            <div className="animate-appear opacity-0 py-2" style={{"--delay":"0.5s"}}>
+                <div className="animate-float" style={{"--delay":"1.5s"}}>
+                    Welcome
+                </div>
+            </div>
+            <div className="animate-appear opacity-0 py-2" style={{ "--delay": "1s" }}>
+                <div className="animate-float" style={{"--delay":"1.75s"}}>
+                    To
+                </div>
+            </div>
+            <div className="animate-appear opacity-0 py-2 uppercase" style={{ "--delay": "1.5s" }}>
+                <div className="animate-float" style={{"--delay":"2s"}}>
+                    The Clock
+                </div>
+            </div>
+        </div>
     )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        appear: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(4rem)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0rem)",
+          },
+        },
+        float: {
+          "0%": {
+            transform: "translateY(0rem)",
+          },
+          "50%": {
+            transform: "translateY(1rem)",
+          },
+          "100%": {
+            transform: "translateY(0rem)",
+          },
+        }
+      },
+      animation: {
+        appear: "appear 1s ease-in-out forwards var(--delay, 0)",
+        float: "float 5s ease-in-out infinite var(--delay, 0)"
+      },
+    },
   },
   plugins: [],
 }
